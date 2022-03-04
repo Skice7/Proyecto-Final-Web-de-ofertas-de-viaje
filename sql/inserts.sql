49f851080ed2a71b7e173f3c18f6d1ab3ef98323
Samuel Zecirovic <55589454+SamuelZecirovic@users.noreply.github.com>
2022-03-04 11:33:17 +0100

    Add files via upload

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -1,8 +1,25 @@
+SET auto_increment_increment = 1;
+
 insert into chollos (titulo,ubicacion,direccion,imagen,precio,descripcion,fecha_caducidad)
-values ("titulo1","ubicacion1","direccion1","imagen1",1,"descripcion1","2000-1-10");
+values 
+("titulo1","ubicacion1","direccion1","imagen1",1,"descripcion1","2000-1-10"),
+("titulo2","ubicacion2","direccion2","imagen2",2,"descripcion2","2000-2-20"),
+("titulo3","ubicacion3","direccion3","imagen3",3,"descripcion3","2000-3-30"),
+("titulo4","ubicacion4","direccion4","imagen4",4,"descripcion4","2000-4-10"),
+("titulo5","ubicacion5","direccion5","imagen5",5,"descripcion5","2000-5-10");
 
 insert into hoteles (direccion,estrellas,id_chollo)
-values ("direccion1",1,1);
+values 
+("direccion1",1,1),
+("direccion2",2,2),
+("direccion3",3,3),
+("direccion4",4,4),
+("direccion5",5,5);
 
 insert into users (nombre,apellidos,dni,correo,telefono)
-values ("nombre1","apellido1","12345678A","correo@correo.com",123456789);
+values 
+("nombre1","apellido1","12345678A","correo1@correo.com",123456789),
+("nombre2","apellido2","22345678A","correo2@correo.com",223456789),
+("nombre3","apellido3","32345678A","correo3@correo.com",323456789),
+("nombre4","apellido4","42345678A","correo4@correo.com",423456789),
+("nombre5","apellido5","52345678A","correo5@correo.com",523456789);
--- sql/tablas.sql
+++ sql/tablas.sql
@@ -1,33 +1,36 @@
 use heroku_99fe8b241e03000;
 
+drop table if exists hoteles;
 drop table if exists chollos;
+drop table if exists users;
+
 create table chollos(
 	id_chollo int primary key auto_increment,
 	titulo varchar(100),
-    ubicacion varchar(255),
-    direccion varchar(255),
+    	ubicacion varchar(255),
+    	direccion varchar(255),
 	imagen varchar(100),
 	precio decimal,
 	descripcion text,
 	fecha_caducidad date
 );
 
-drop table if exists hoteles;
+
 create table hoteles(
 	id_hotel int primary key auto_increment,
 	direccion varchar(255),
 	estrellas int,
 	id_chollo int,
-    constraint id_chollo foreign key (id_chollo) references chollos (id_chollo)
+	constraint id_chollo foreign key (id_chollo) references chollos (id_chollo) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-drop table if exists users;
+
 create table users(
 	id_user int primary key auto_increment,
 	nombre varchar(100),
-    apellidos varchar(255),
-    dni varchar(9),
-    correo varchar(255),
-    telefono int
+    	apellidos varchar(255),
+    	dni varchar(9),
+    	correo varchar(255),
+    	telefono int
 );
 
