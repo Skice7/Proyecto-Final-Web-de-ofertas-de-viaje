e264cabccb5bc06807e2388ffd80302d79413413
Samuel Zecirovic <55589454+SamuelZecirovic@users.noreply.github.com>
2022-03-29 09:55:06 +0200

    commit 4

--- Proyecto-final/src/main/java/MainApp/dao/IChollosDAO.java
+++
@@ -1,9 +0,0 @@
-package MainApp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import MainApp.dto.Chollos;
-
-public interface IChollosDAO extends JpaRepository<Chollos, Integer>{
-
-}
--- Proyecto-final/src/main/java/MainApp/dao/IHotel_cholloDAO.java
+++
@@ -1,9 +0,0 @@
-package MainApp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import MainApp.dto.Hotel_chollo;
-
-public interface IHotel_cholloDAO extends JpaRepository<Hotel_chollo, Integer>{
-
-}
